4957cadae89ed8d4148ce44261f2e074be7dce8f
ajspryn <adjisupriyono@gmail.com>
2023-02-18 05:26:28 +0700

    perbaikan

--- config/app.php
+++ config/app.php
@@ -181,13 +181,13 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Laravolt\Avatar\ServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
         * Package Service Providers...
         */
-        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
-        Laravolt\Avatar\ServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
